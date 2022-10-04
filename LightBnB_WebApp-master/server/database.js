0c0015f9565716925d08fa239a0a6f6501892c62
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-10-04 11:57:58 -0700

    Refactor addProperty

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -41,8 +41,8 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
-  const value = [user.name, user.password, user.email];
-  return pool.query(`INSERT INTO users (name, password, email) values ($1, $2, $3) RETURNING *;`, value)
+  const queryValue = [user.name, user.password, user.email];
+  return pool.query(`INSERT INTO users (name, password, email) values ($1, $2, $3) RETURNING *;`, queryValue)
   .then(result => result.rows[0])
   .catch(err => console.log(err.message));
 }
@@ -133,9 +133,33 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryValues = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+  ];
+
+  return pool.query(`
+  INSERT INTO properties
+  (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `, queryValues)
+    .then(result => {
+      cosole.log(result.row);
+      return result.rows;
+    })
+    .catch(err => err.message);
 }
 exports.addProperty = addProperty;
