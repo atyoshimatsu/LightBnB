635c766d59494824777778e893aadcd590d69a07
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-10-04 00:21:06 -0700

    Implement getUser query, addUser,  and getAllreservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,16 +16,9 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query(`SELECT * FROM users WHERE email=$1`, [email])
+  .then(result => result.rows[0])
+  .catch(err => console.log(err.message));
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -35,7 +28,9 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`SELECT * FROM users WHERE id=$1`, [id])
+  .then(result => result.rows[0])
+  .catch(err => console.log(err.message));
 }
 exports.getUserWithId = getUserWithId;
 
@@ -45,11 +40,11 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+const addUser = function(user) {
+  const value = [user.name, user.password, user.email];
+  return pool.query(`INSERT INTO users (name, password, email) values ($1, $2, $3) RETURNING *;`, value)
+  .then(result => result.rows[0])
+  .catch(err => console.log(err.message));
 }
 exports.addUser = addUser;
 
@@ -61,7 +56,14 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+SELECT * FROM reseravations AS r
+JOIN properties AS p ON p.id = r.property_id
+WHERE r.guest_id = $1
+LIMIT $2
+  `, [guest_id, limit])
+  .then(result => result.rows)
+  .catch(err => err.message);
 }
 exports.getAllReservations = getAllReservations;
 
